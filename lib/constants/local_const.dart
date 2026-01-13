c146a635b0acf462770186182ca10386804e3be9
shailesh43 <satheshailesh43@gmail.com>
2026-01-13 11:50:29 +0530

    add: shared constant

---
+++ lib/constants/local_const.dart
@@ -0,0 +1,47 @@
+import 'package:shared_preferences/shared_preferences.dart';
+
+class LocalPreferances {
+  static const int preftypeInt = 0;
+  static const int preftypeString = 1;
+  static const int preftypeBool = 2;
+
+  static saveValues(type, key, value) async {
+    final prefs = await SharedPreferences.getInstance();
+    if (type == preftypeInt) {
+      await prefs.setInt(key, value);
+    } else if (type == preftypeString) {
+      await prefs.setString(key, value);
+    } else if (type == preftypeBool) {
+      await prefs.setBool(key, value);
+    }
+  }
+
+  static Future<bool> getBoolValues(key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    bool boolValue = prefs.getBool(key) ?? false;
+    return boolValue;
+  }
+
+  static Future<int> getIntValues(type, key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    int intValue = prefs.getInt(key) ?? 0;
+    return intValue;
+  }
+
+  static Future<String> getStringValues(key) async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    String stringValue = prefs.getString(key) ?? "";
+    return stringValue;
+  }
+
+  static Future<String> getStringValuesNew(key) {
+    return SharedPreferences.getInstance().then((prefs) {
+      String stringValue = prefs.getString(key) ?? "";
+      return stringValue;
+    });
+  }
+
+  void saveEmployeeCode(String? sAPEMPNO) {
+    saveValues(1, "empCode", sAPEMPNO);
+  }
+}
---
+++ lib/constants/pref_const.dart
@@ -0,0 +1,18 @@
+class PreferenceConstants {
+  static const String empNo = "EMP_NO";
+  static const String empShortName = "EMP_SHORT_NAME";
+  static const String empDisplayName = "EMP_DISPLAY_NAME";
+  static const String empOrg = "EMP_ORG";
+  static const String empCostCenter = "EMP_COST_CENTER";
+  static const String empCostCenterDescr = "EMP_COST_CENTER_DESCR";
+  static const String empDesignation = "EMP_DESIGNATION";
+  static const String empEmail = "EMP_EMAIL";
+  static const String empWorkLocation = "EMP_WORK_LOCATION";
+  static const String empCompanyDescr = "EMP_COMPANY_DESCR";
+  static const String empDoj = "EMP_DOJ";
+  static const String empDob = "EMP_DOB";
+  static const String empMobile = "EMP_MOBILE";
+  static const String empRole = "EMP_ROLE";
+  static const String fcmToken = "FCM_TOKEN";
+  static const String isLoggedIn = "IS_LOGGED_IN";
+}
